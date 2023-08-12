4a9ff6f0df38af53d0ff6a7a8d7abea265606786
jhkang <gogo0920007@gmail.com>
2023-08-12 11:55:34 +0900

    feat: openai model list 확인

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import os
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
 from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+import os
+import openai
+
+load_dotenv()
 
 
 def table_to_markdown(table):
-    headers = [cell['text'] for cell in table['columns']]
+    headers = [cell["text"] for cell in table["columns"]]
     header_row = "| " + " | ".join(headers) + " |"
     separator_row = "| " + " | ".join(["---"] * len(headers)) + " |"
 
     rows = []
-    for row in table['rows']:
-        row_data = [cell['text'] for cell in row]
+    for row in table["rows"]:
+        row_data = [cell["text"] for cell in row]
         rows.append("| " + " | ".join(row_data) + " |")
 
     markdown_table = "\n".join([header_row, separator_row] + rows)
@@ -19,6 +24,8 @@ def table_to_markdown(table):
 
 
 def analyze_doc(image_path):
+    endpoint = os.getenv("FORM_URL")
+    key = os.getenv("FORM_API_KEY")
     document_analysis_client = DocumentAnalysisClient(
         endpoint=endpoint, credential=AzureKeyCredential(key)
     )
@@ -29,16 +36,26 @@ def analyze_doc(image_path):
     result = poller.result()
 
     full_contents = result.content
+    markdown_tables = []
     if len(result.tables):
-        markdown_tables = []
         for table in result.tables:
             markdown_tables.append(table_to_markdown(table))
 
+    return full_contents + "\n".join(markdown_tables)
+
+
+def get_model():
+    openai.organization = os.getenv("OPENAI_ORG")
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    model_list = [x['id'] for x in openai.Model.list()['data']]
+    for model_name in model_list:
+        print(model_name)
+    # chat = ChatOpenAI(temperature=0)
+
 
 if __name__ == "__main__":
-    load_dotenv()
-    endpoint = os.getenv("FORM_URL")
-    key = os.getenv("FORM_API_KEY")
+    image_path = "img/test_1.jpg"
+    # contents = analyze_doc(image_path)
+    # print(contents)
 
-    image_path = "/Users/jhkang/PycharmProjects/olive/img/test_1.jpg"
-    analyze_doc(image_path)
+    get_model()
